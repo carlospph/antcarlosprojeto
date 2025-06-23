c75f07fec836e5587be9566b918adf556b3956c9
home <80932010+carlospph@users.noreply.github.com>
2025-06-23 14:15:42 +0000

    Addicionando styled-components ao botao entrar

--- src/components/actionUser/ActionUser.jsx
+++ src/components/actionUser/ActionUser.jsx
@@ -1,11 +1,46 @@
 import { Link } from "react-router-dom";
-import styles from "./ActionUser.module.css";
+
+import styles from "styled-components";
+
+const Container = styles.div`
+	display: flex;
+	align-items: center;
+	gap: 20px;
+	justify-content: center;
+
+		@media(max-width: 768px){
+			align-items: center;
+			justify-content: start;
+		}
+`;
+
+const ItemLink = styles.span`
+	text-decoration:underline;
+`;
+
+const ItemBtn = styles.button`
+	background-color: var(--primary);
+	color: white;
+	border-radius:6px;
+	border:0;
+	padding: .5em 2rem;
+
+	&:hover{
+		cursor: pointer;
+		opacity:.8;
+		transition: .3s;
+	}
+`;
 
 export const ActionUser = () => {
-	return(
-		<div className={styles.flex__actionUser}>
-			 <Link to="/NewUser" className={styles.link__register}> Cadastre-se</Link>
-			 <Link to="/Login" className={styles.link__btn}> Entrar </Link>
-		</div>
-	)
-}
+  return (
+    <Container>
+      <Link to="/NewUser">
+        <ItemLink>Cadastre-se</ItemLink>
+      </Link>
+      <Link to="/Login">
+        <ItemBtn>Entrar</ItemBtn>
+      </Link>
+    </Container>
+  );
+};
