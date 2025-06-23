2a422e62dd3fdf16f9b58109e0c1b2749c6ee7e3
Carlos Nascimento <80932010+carlospph@users.noreply.github.com>
2025-06-23 09:38:30 -0300

    Update MobileMenu.jsx
    add actionuser

--- src/components/header/MobileMenu.jsx
+++ src/components/header/MobileMenu.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import {} from '../components/actionUser/ActionUser.jsx';
+import {} from '../../actionUser/ActionUser.jsx';
 import './Header.css';
  
 export const MobileMenu = () => {
